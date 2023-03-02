bf952e35160e69bd62e39840621ba22b37c31738
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2023-03-02 22:07:25 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(randint(0, 10))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+})
+basic.forever(function () {
+	
+})
